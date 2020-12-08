e5baa86e234f48b66f3719eaf36a25f178f31345
Chris7c0 <chris7c0@gmail.com>
2020-12-08 13:39:52 -0600

    Updating batch files, moving XPMod.sp

--- LaunchServerAndCompile.bat
+++ LaunchServerAndCompile.bat
@@ -3,7 +3,7 @@
 set l4d2pluginfolder="C:\Program Files (x86)\Steam\steamapps\common\Left 4 Dead 2 Dedicated Server\left4dead2\addons\sourcemod\plugins\"
 set l4d2networkfolder="D:\l4d2\"
 
-spcomp xpmod.sp -v 0
+spcomp .\XPMod\xpmod.sp -v 0
 
 @echo off
 
