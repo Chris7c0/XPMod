06ec7d6048ada15caf031d7d36147d71a3e76f19
ChrisP <chris7c0@gmail.com>
2021-12-19 00:05:31 -0600

    Made the new helper bash script automatically copy the updated smx into the plugins folder on the local test server.

--- xpmodcompile.sh
+++ xpmodcompile.sh
@@ -3,6 +3,9 @@
 # compile xpmod
 ./spcomp xpmod.sp -v:0
 
+# Copy the new smx file into the test server
+cp ./xpmod.smx /home/steam/l4d2testing/left4dead2/addons/sourcemod/plugins/
+
 # Print the timestamp of the build completion
 timestamp=$(date +%Y-%m-%d\ %H:%M:%S)
 echo "                                     " $timestamp
